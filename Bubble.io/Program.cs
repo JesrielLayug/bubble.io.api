6ad0dd5b18daddb69034ac645f0b0b3800e8c34d
JesrielLayug <layugjesriel61@gmail.com>
2024-03-08 01:20:47 +0800

    remove password digit.

--- Bubble.io/Program.cs
+++ Bubble.io/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddIdentityApiEndpoints<IdentityUser>()
 
 builder.Services.Configure<IdentityOptions>(options =>
 {
-    options.Password.RequireDigit = true;
+    options.Password.RequireDigit = false;
     options.Password.RequireLowercase = true;
     options.Password.RequireUppercase = false;
     options.Password.RequireNonAlphanumeric = false;
